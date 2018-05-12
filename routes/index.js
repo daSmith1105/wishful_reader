b5dd6668476cce621878ae28f286b1cef2ccdc3c
daSmith1105 <mustangdas@gmail.com>
2018-05-11 19:52:54 -0500

    removed index router

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '../public/index.html');
-});
-module.exports = router;
